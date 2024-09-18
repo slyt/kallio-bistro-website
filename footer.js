2716724a1876be05a1930cd21ca52d5b55553cd6
Taylor Sly <you@example.com>
2024-09-18 02:16:06 -0500

    remove footer.js

--- footer.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    fetch('footer.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('footer').innerHTML = data; // Inject footer content into the specified div
-        })
-        .catch(error => console.error('Error fetching footer:', error));
-});
